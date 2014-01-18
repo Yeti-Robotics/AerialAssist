de05e2e4011c170f85c296a5d210d945a1162b04
nacortes7 <monkeybiznes@carolina.rr.com>
2014-01-18 12:59:41 -0500

    Added basic framework
    AND ALL DAAAT #YOLO SWAG

--- src/edu/wpi/first/wpilibj/templates/Catapult.java
+++ src/edu/wpi/first/wpilibj/templates/Catapult.java
@@ -3,21 +3,32 @@
  */
 
 
-/*    ##   ##       YYYY    YYYY      OOOOO     LLLLLL           OOOOO              SSSSSSSS    WWWWW            WWWWW     AAAAAAA           GGGGGGGG  
-      ##   ##        YY      YY     OO     OO     LL           OO     OO           SSS     SS    WW                WW       AA AA         GGGG      GGG
-   #############      YY    YY     OOO     OOO    LL          OOO     OOO          SSSS           WW              WW       AA   AA       GGG        
-      ##   ##           YYYY       OOO     OOO    LL          OOO     OOO             SSSSS        WW    WWWW    WW       AAAAAAAAA      GG
-   #############         YY        OOO     OOO    LL          OOO     OOO                SSSS       WW WW   WW WW        AA       AA     GGG       GGGGGG
-      ##   ##            YY         OO     OO     LL     LL    OO     OO           SS     SSS        WWW      WWW       AA         AA     GGGG      GG GG
-      ##   ##          YYYYYY         OOOOO      LLLLLLLLLL      OOOOO              SSSSSSSS       WWWWWWW  WWWWWWW   AAAAA       AAAAA     GGGGGGGGG  GG
+/*     ##   ##       YYYY    YYYY      OOOOO      LLLLL            OOOOO              SSSSSSSS    WWWWW            WWWWW     AAAAAAA           GGGGGGGG  
+       ##   ##        YY      YY     OO     OO     LLL           OO     OO           SSS     SS    WW                WW       AAAAA         GGGG      GGG
+   #############       YY    YY     OOO     OOO    LLL          OOO     OOO          SSSS           WW              WW       AA   AA       GGGGG        
+      ##   ##            YYYY       OOO     OOO    LLL          OOO     OOO             SSSSS        WW    WWWW    WW       AAAAAAAAA      GGGGG
+   #############          YY        OOO     OOO    LLL          OOO     OOO                SSSS       WW WW    WW WW       AA       AA     GGGGG     GGGGGG
+     ##   ##              YY         OO     OO     LLL     LL    OO     OO           SS     SSS        WWW      WWW       AA         AA     GGGG      GG GG
+     ##   ##            YYYYYY         OOOOO      LLLLLLLLLL       OOOOO              SSSSSSSS       WWWWWWW  WWWWWWW   AAAAA       AAAAA     GGGGGGGGG  GG
 */
 
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Solenoid;
+
 public class Catapult {
     
-    public static void Catapult(int sol1, int sol2, boolean limitSwitchCatapult){
-        
+    Solenoid solenoid1;
+    Solenoid solenoid2;
+    DigitalInput armDownLimit; 
+    
+    
+    
+    public void Catapult(int sol1, int sol2, int limitSwitchPort){
+        solenoid1 = new Solenoid(sol1);
+        solenoid2 = new Solenoid(sol2);
+        armDownLimit = new DigitalInput(limitSwitchPort);
     }
     
     public boolean isDown(){
@@ -35,14 +46,6 @@ public class Catapult {
     }
     
     public void shootLow(){
-    
-    }
-    
-    public void raiseArm(){
-    
-    }
-    
-    public void lowerArm(){
     
     }
 }
