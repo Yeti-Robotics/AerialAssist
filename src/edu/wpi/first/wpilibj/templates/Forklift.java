216937757d029494acbb8602b6caf6deda4d934c
samperlmutter <sam.perlmutter227@gmail.com>
2014-01-18 11:48:30 -0500

    Added constructor method for forklift

--- src/edu/wpi/first/wpilibj/templates/Forklift.java
+++ src/edu/wpi/first/wpilibj/templates/Forklift.java
@@ -5,11 +5,27 @@
  */
 
 package edu.wpi.first.wpilibj.templates;
-
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.DigitalInput;
 /**
  *
  * @author Sam
  */
+
 public class Forklift {
-    
+    Jaguar speedController;
+    DigitalInput upLimit;
+    DigitalInput downLimit;
+    DigitalInput midLimit;
+
+    public void Forklift(int speedControllerPort, int upLimitPort, int downLimitPort, int midLimitPort) {
+        speedController = new Jaguar(speedControllerPort);
+        upLimit = new DigitalInput(upLimitPort);
+        downLimit = new DigitalInput(downLimitPort);
+        midLimit = new DigitalInput(midLimitPort);
+    }
+    public void move(){}
+    public void moveUp(){}
+    public void moveDown(){}
+    public void moveMiddle(){}
 }
