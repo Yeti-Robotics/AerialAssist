3c888cbb5320973e480eb0d5171dc27a1c9eb24f
Amy Waldron <Amythewhovianchick@gmail.com>
2014-01-18 13:12:45 -0500

    Created basic framework for the drivetrain.

--- src/edu/wpi/first/wpilibj/templates/DriveTrain.java
+++ src/edu/wpi/first/wpilibj/templates/DriveTrain.java
@@ -3,13 +3,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+/*
 package edu.wpi.first.wpilibj.templates;
 
 /**
  *
  * @author Amy
  */
+
+import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Jaguar;
+
+
 public class DriveTrain {
+    Jaguar frontLeft;
+    Jaguar backLeft;
+    Jaguar frontRight;
+    Jaguar backRight;
     
+    public void DriveTrain(int frontLeftPort, int frontRightPort, int backLeftPort, int backRightPort) {
+        frontLeft = new Jaguar(frontLeftPort);
+        frontRight = new Jaguar(frontRightPort);
+        backLeft = new Jaguar(backLeftPort);
+        backRight = new Jaguar(backRightPort);
+    }
+    public void frontRight() {
+     
+        
+        
+    }
+    public void frontLeft() {
+        
+        
+        
+    }   
+    public void backRight() {
+        
+        
+        
+    }
+    public void backLeft() {
+        
+        
+        
+    }
+       
+    }    
 }
