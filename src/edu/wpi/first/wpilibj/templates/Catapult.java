2098e3e9574733f69399eb5bd2f0011f445d5775
nacortes7 <monkeybiznes@carolina.rr.com>
2014-01-18 09:54:48 -0500

    Added a catapult class or something

---
+++ src/edu/wpi/first/wpilibj/templates/Catapult.java
@@ -0,0 +1,28 @@
+/*
+ * This class controls the shooting arm on the roboto
+ */
+
+
+/*  SSSSSSSS    WWWWW            WWWWW
+   SSS     SS    WW                WW
+   SSSS          WW                WW
+      SSSSS       WW     WWWW     WW
+         SSSS      WWW WWW  WWW WWW
+   SS     SSS       WWWWW    WWWWW
+    SSSSSSSS       WWWWWWW  WWWWWWW   
+*/
+
+
+
+
+
+
+
+package edu.wpi.first.wpilibj.templates;
+
+public class Catapult {
+    
+    public static void method(){
+        
+    }
+}
