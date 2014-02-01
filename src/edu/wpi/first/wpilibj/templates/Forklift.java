242dfce01c85ec75c82b21d808ddb9968eac37d1
samperlmutter <sam.perlmutter227@gmail.com>
2014-02-01 15:12:30 -0500

    finished fork code
    finished fork code that successfully uses 2 buttons to comfortably
    control the fork, also added the beginnings of a possible 3rd limit
    switch.

--- src/edu/wpi/first/wpilibj/templates/Forklift.java
+++ src/edu/wpi/first/wpilibj/templates/Forklift.java
@@ -1,31 +1,117 @@
 /*
+
+
+
+
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 package edu.wpi.first.wpilibj.templates;
-import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Talon;
+
 /**
  *
  * @author Sam
  */
 
 public class Forklift {
-    Jaguar speedController;
-    DigitalInput upLimit;
-    DigitalInput downLimit;
-    DigitalInput midLimit;
+    DigitalInput upLimit = new DigitalInput(1);
+    DigitalInput midLimit = new DigitalInput(3);
+    DigitalInput downLimit = new DigitalInput(6);
+    Talon forkTalon = new Talon(7);
+    boolean aboveMiddle;
 
-    public void Forklift(int speedControllerPort, int upLimitPort, int downLimitPort, int midLimitPort) {
-        speedController = new Jaguar(speedControllerPort);
-        upLimit = new DigitalInput(upLimitPort);
-        downLimit = new DigitalInput(downLimitPort);
-        midLimit = new DigitalInput(midLimitPort);
+    public void Forklift() {
+       aboveMiddle = true;
+        
     }
-    public void move(){}
-    public void moveUp(){}
-    public void moveDown(){}
-    public void moveMiddle(){}
+   // public void forkMove(){
+        
+        
+  /*      if (downLimit.get())
+        {
+            if (midJoy.getTrigger())
+            {
+                forkTalon.set(.1);
+            }
+            else 
+            {
+                forkTalon.set(0);
+            }
+        } else if (midLimit.get()) {
+            
+            forkTalon.set(0);
+            
+            if (midJoy.getTrigger()) {
+                
+                forkTalon.set(.1);
+                
+            } 
+            else if (leftJoy.getTrigger()) {
+                forkTalon.set(-0.05);
+            } 
+            else {
+                forkTalon.set(0);
+            }
+            }
+        
+        else if (upLimit.get()) 
+        {  
+            if(leftJoy.getTrigger())
+            {
+                forkTalon.set(-.05);
+            }
+            else 
+            {
+                forkTalon.set(0);
+            } 
+        } 
+        else
+        {if (leftJoy.getTrigger()) 
+            {
+                forkTalon.set(-.05);   
+            }
+            else
+            {
+               forkTalon.set(0); 
+            }
+         }*/
+    
+    public void forkMoveUp() {
+        //upLimit is port 1
+    if (downLimit.get()== false) {
+        forkTalon.set(0);
+    }
+    else {
+        forkTalon.set(-1);
+    }
+    }
+
+    public void forkMoveDown(){
+        //downLimit is port 6
+    if (upLimit.get()== false) {
+        forkTalon.set(0);
+    }
+    else {
+        forkTalon.set(1);
+    }
+    }
+    public void forkMoveMiddle(){}
+    
+    public void forkAboveMiddle(){
+    if (midLimit.get()== false) {
+        forkTalon.set(0.5);
+    }
+    }
+    public void forkBelowMiddle(){
+    if (midLimit.get()== false) {
+        forkTalon.set(-1);
+    }
+    }
+    public void stop(){
+    forkTalon.set(0);
+    }
 }
